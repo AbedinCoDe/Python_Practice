9629d68bc62291c9571139835058eefebcacaf19
AbCoDe67 <mdkausarul.abedin@gmail.com>
2023-01-19 11:29:04 +0600

    Final Commit

--- Simple_Calculator/Calculator.py
+++ Simple_Calculator/Calculator.py
@@ -1,4 +1,8 @@
 
+from Logo import logo
+
+
+print(logo)
 
 # Add
 def add(n1, n2):
@@ -24,24 +28,31 @@ operation = {
     "/": divide
 }
 # first calculation
+def calculator():
+    num1 = int(input("What is Your 1st number? \n"))
 
-should_continue = True
+    should_continue = True
+    while should_continue:
+        for operate in operation:
+            print(operate)
+        operation_symble = input("what is your operation? \n")
+        num2 = int(input("What is the next number?\n"))
 
-while should_continue:
-    num1 = int(input("What is Your 1st number? "))
-    for operate in operation:
-        print(operate)
-    operation_symble = input("what is your operation? \n")
-    num2 = int(input("What is the next number?\n"))
+        calculation_function = operation[operation_symble]
+        first_answer = calculation_function(num1, num2)
+        print(f"Your answer is: {first_answer}")
 
-    calculation_function = operation[operation_symble]
-    first_answer = calculation_function(num1, num2)
-    print(f"Your answer is: {first_answer}")
+        # for second calculation
+        check_Input = input(f"If you want to calculte more number with {first_answer} then type 'Y' And stop 'N' \n")
 
-    # for second calculation
+        if check_Input == "N":
+            should_continue = False
+            calculator()
 
-    if (f"If you want to calculte more number with ")
+        elif  check_Input == "Y":
+            num1 = first_answer
 
+calculator()
 
 
 
